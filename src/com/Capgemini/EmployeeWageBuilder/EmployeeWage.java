87f62a2979971c190fea55dadd71f8ff07c7e572
Amit Kumar <amitnitsri25@gmail.com>
2020-10-22 17:23:50 +0530

    Employee Wage Builder Day4

---
+++ src/com/Capgemini/EmployeeWageBuilder/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.Capgemini.EmployeeWageBuilder;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Builder");
+	}
+
+}
