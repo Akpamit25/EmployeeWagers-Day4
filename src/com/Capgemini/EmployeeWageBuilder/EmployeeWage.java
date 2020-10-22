8e16ea5e14460c2a947aa564baf2f7afca1c5ffc
Amit Kumar <amitnitsri25@gmail.com>
2020-10-22 19:11:32 +0530

    UC8 : Computing Employee Wage For Multiple Companies

--- src/com/Capgemini/EmployeeWageBuilder/EmployeeWage.java
+++ src/com/Capgemini/EmployeeWageBuilder/EmployeeWage.java
@@ -1,60 +1,68 @@
-
 package com.Capgemini.EmployeeWageBuilder;
 
 public class EmployeeWage {
-	public static int empWage_per_hour = 20;
-	public static int max_working_days = 20;
-	public static int max_hours_of_work = 100;
-static int count = 1;
-	public static int totalEmployeeWage = 0;
-	public static int totalWorkingHour = 0;
 
-	public static void main(String[] args) {
+	private String companyName;
+	private final int empWage_per_hour;
+	private final int max_working_days;
+	private final int max_hours_of_work;
 
-		checkEmployee(); // checks whether employee is present or not
-		System.out.println("\n Total Employee Wage = " + totalEmployeeWage);
-		System.out.println("\n Total Working Hour : " + totalWorkingHour);
+	public EmployeeWage(String companyName, int empWage_per_hour, int max_working_days, int max_hours_of_work) {
+		super();
+		this.companyName = companyName;
+		this.empWage_per_hour = empWage_per_hour;
+		this.max_working_days = max_working_days;
+		this.max_hours_of_work = max_hours_of_work;
 	}
 
-	public static void checkEmployee() {
+	public static void main(String[] args) {
+		EmployeeWage Reliance = new EmployeeWage("Reliance", 20, 20, 100);
+		Reliance.checkEmployee();
+		EmployeeWage Vmart = new EmployeeWage("Vmart", 22, 18, 50);
+		Vmart.checkEmployee();
+	}
+
+	public void checkEmployee() {
 		int empHour = 0;
+		int totalEmployeeWage = 0;
+		int totalWorkingHour = 0;
+		System.out.println(
+				"******************  Details Of " + this.companyName + " Starts From Here  ******************\n\n");
+
 		for (int day = 1; day <= max_working_days; day++) {
 			if (totalWorkingHour <= max_hours_of_work) {
 				int x = (int) (Math.random() * 3);
 				switch (x) { // Using Switch Case
 				case 0:
-					System.out.print("Day No. "+day+", ");
+					System.out.print("Day No. " + day + ", ");
 					System.out.print("Employee Is Absent");
 					empHour = 0;
-					dailyEmployeeWage(empHour);
 					break;
 
 				case 1:
-					System.out.print("Day No. "+day+", ");
+					System.out.print("Day No. " + day + ", ");
 					System.out.print("Employee Is Present");
 					empHour = 8;
-					dailyEmployeeWage(empHour);
 					break;
 
 				case 2:
-					System.out.print("Day No. "+day+", ");
+					System.out.print("Day No. " + day + ", ");
 					System.out.print("Part-Time Work");
 					empHour = 4;
-					dailyEmployeeWage(empHour);
 					break;
-
 				}
+
+				totalWorkingHour += empHour;
+				System.out.print("\nDay " + day + " Employee Hour = " + empHour + ", " + "Total Working Hour Till Day "
+						+ day + " =" + totalWorkingHour + "\n\n");
+				int wage = empHour * empWage_per_hour;
+				totalEmployeeWage += wage;
 			}
 		}
-	}
-
-	public static void dailyEmployeeWage(int hour) {
-		totalWorkingHour += hour;
-		System.out.print("\nDay "+count+" Employee Hour = " +hour+", "+"Total Working Hour Till Day "+count+" ="+ totalWorkingHour +"\n\n");
-		int wage = hour * empWage_per_hour;
-		totalEmployeeWage += wage;
-		count++;
+		System.out.println("\n Total Monthly Employee Wage For " + this.companyName + "=" + totalEmployeeWage);
+		System.out.println(" Total Monthly Working Hour For " + this.companyName + "=" + totalWorkingHour + "\n\n");
+		System.out
+				.println("******************  Details Of " + this.companyName + " Ends Here  ******************\n\n\n");
 
 	}
-
 }
